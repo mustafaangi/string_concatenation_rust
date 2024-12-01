6f254ef4e501c71b973a87e0be17622fe48b388b
Mustafa Angı <mustafaangi@192.168.1.100>
2024-12-01 15:06:08 +0300

    Add target/ to .gitignore

--- target/debug/deps/string_concatenation-ba93f2dc71561227.d
+++
@@ -1,5 +0,0 @@
-/Users/mustafaangi/my_project/string_concatenation/target/debug/deps/string_concatenation-ba93f2dc71561227: src/main.rs
-
-/Users/mustafaangi/my_project/string_concatenation/target/debug/deps/string_concatenation-ba93f2dc71561227.d: src/main.rs
-
-src/main.rs:
--- target/debug/string_concatenation.d
+++
@@ -1,1 +0,0 @@
-/Users/mustafaangi/my_project/string_concatenation/target/debug/string_concatenation: /Users/mustafaangi/my_project/string_concatenation/src/main.rs
